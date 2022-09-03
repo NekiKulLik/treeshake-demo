e8432be775050e2d15338db22e2bf1f59cfcbc85
NekiKulLik <63129666+NekiKulLik@users.noreply.github.com>
2022-09-03 13:43:57 +0200

    initial code

---
+++ plugins/vue-chartjs.plugin.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import { Scatter } from 'vue-chartjs/legacy'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  LineElement,
+  PointElement
+} from 'chart.js'
+
+export default () => {
+  ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    PointElement,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    LineElement
+  )
+
+  Vue.component('ScatterChart', {
+    extends: Scatter
+  })
+}
